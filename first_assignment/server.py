782cc812976609d8d0a77f2bf012562dd853e3c7
VladFearo <69718459+VladFearo@users.noreply.github.com>
2024-06-06 13:10:44 +0300

    ADD docstrings for all the functions and hadeling for the case of user isn't signed in yet

--- first_assignment/server.py
+++ first_assignment/server.py
@@ -9,7 +9,16 @@ sock.bind((UDP_IP, UDP_PORT))
 name_pool = dict()
 
 
-def check_message(message):
+def check_message_type(message):
+    """
+    Checks to find the type of the message, tyeps can be of either Name or Name and message
+
+    Args:
+        message (String): the message recieved from the user
+
+    Returns:
+        String: The type of the message
+    """
     parts = message.split(' ', 1)
     if len(parts) == 1:
         return "Name"
@@ -20,33 +29,48 @@ def check_message(message):
 
 
 def find_sender(address):
+    """
+    Checks the name pool to find a sender for a given address otherwise returns an empty string
+
+    Args:
+        address (String): The ip address and port of the user we're trying to find
+
+    Returns:
+        String: The name of the user found or an empty string if the user is not found.
+    """
     for name, addr in name_pool.items():
         if addr == address:
             return name
+    # if user not found return an empty string
+    return ""
 
 
 while True:
     try:
         data, addr = sock.recvfrom(1024)
         message = data.decode()
-        type = check_message(message)
-        if type == "Name":
+        msg_type = check_message_type(message)
+        if msg_type == "Name":
             name = message
             if name not in name_pool:
                 name_pool[name] = addr
                 print("added", name, addr)
                 sock.sendto("Name Added".encode(), addr)
 
-        elif type == "Name and message":
+        elif msg_type == "Name and message":
 
             # TODO: Check what to do if nameless user.
+            sender = find_sender(name_pool[name])
+            if sender == "":
+                sock.sendto("Users need to sign up before sending messages".encode(), addr)
+                break
             name, message_content = message.split(' ', 1)
             print('received message:', name, addr)
             if name not in name_pool:
                 sock.sendto("User doesn't exist".encode(), addr)
 
             elif name in name_pool:
-                sender = find_sender(name_pool[name])
+                
                 sock.sendto(f"{sender}: {message_content}".encode(), name_pool[name])
                 sock.sendto("Message sent".encode(), addr)
                 print("message sent")
